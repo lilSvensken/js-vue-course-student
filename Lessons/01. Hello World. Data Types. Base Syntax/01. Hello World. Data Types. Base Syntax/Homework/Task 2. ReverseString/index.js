fe03d762b34921e8894541d4068bda2d1c579d04
SofyaMelnikova <melsofa@mail.ru>
2023-09-09 17:45:30 +0300

    Made all homework

--- Lessons/01. Hello World. Data Types. Base Syntax/01. Hello World. Data Types. Base Syntax/Homework/Task 2. ReverseString/index.js
+++ Lessons/01. Hello World. Data Types. Base Syntax/01. Hello World. Data Types. Base Syntax/Homework/Task 2. ReverseString/index.js
@@ -1,6 +1,16 @@
 const reverseString = (str) => {
     // Начало
+    function rec(str, newStr) {
+        if (str.length === 1) {
+            return newStr += str;
+        } else {
+            newStr += str[str.length - 1];
+            rec(str.slice(0, str.length - 1), newStr);
+        }
+    }
 
+    let newStr = "";
+    return rec(str, newStr);
     // Конец
 };
 
--- Lessons/01. Hello World. Data Types. Base Syntax/01. Hello World. Data Types. Base Syntax/Homework/Task 3. SumSquareDifference/index.js
+++ Lessons/01. Hello World. Data Types. Base Syntax/01. Hello World. Data Types. Base Syntax/Homework/Task 3. SumSquareDifference/index.js
@@ -1,6 +1,15 @@
 const sumSquareDifference = (num) => {
     // Начало
-    
+    let sumSquare = 0;
+    let squareSum = 0;
+    if (num > 0) {
+        for (let i = 1; i <= num; i++) {
+            sumSquare += i ** 2;
+            squareSum += i;
+        }
+    }
+    squareSum **= 2;
+    return squareSum - sumSquare;
     // Конец
 };
 
