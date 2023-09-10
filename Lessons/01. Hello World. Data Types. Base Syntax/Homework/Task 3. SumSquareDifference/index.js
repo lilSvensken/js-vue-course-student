ff1f686e2313b329c61d6ac48812296c139d5a82
Elina <Elinanurgal@mail.ru>
2023-09-10 12:04:41 +0300

    added 3 task

--- Lessons/01. Hello World. Data Types. Base Syntax/Homework/Task 3. SumSquareDifference/index.js
+++ Lessons/01. Hello World. Data Types. Base Syntax/Homework/Task 3. SumSquareDifference/index.js
@@ -1,7 +1,10 @@
 const sumSquareDifference = (num) => {
-    // Начало
-    
-    // Конец
+    let sum1 = 0, sum2 = 0;
+    while(num !== 0){
+        sum1 += num*num;
+        sum2 += num;
+        num -= 1;
+    }
+    return sum2 * sum2 - sum1;
 };
-
 export default sumSquareDifference;
