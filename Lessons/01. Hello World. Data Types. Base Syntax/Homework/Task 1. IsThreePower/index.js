ec1e35d5f86c4bb20a5117fc32bbef203f3851e9
Elina <Elinanurgal@mail.ru>
2023-09-10 17:26:53 +0300

    updated 1 task

--- Lessons/01. Hello World. Data Types. Base Syntax/Homework/Task 1. IsThreePower/index.js
+++ Lessons/01. Hello World. Data Types. Base Syntax/Homework/Task 1. IsThreePower/index.js
@@ -1,10 +1,14 @@
 const isThreePower = (num) => {
-    if (num <= 0) {
+    if (num > 0) {
+        for (let i = 0; i <= Math.sqrt(num); i++) {
+            if (Math.pow(3, i) === num) {
+                return true;
+            }
+        }
         return false;
     } else {
-        for (let i = 0; i <= Math.sqrt(num); i++) {
-            return Math.pow(3, i) === num;
-        }
+        return false;
     }
 };
-export default isThreePower;
+console.log(isThreePower(0));
+//export default isThreePower;
