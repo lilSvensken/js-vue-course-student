af914fe610d8fc1385c12590ad9495a91ad22d28
Camilo <andrespisa4@gmail.com>
2023-09-15 00:25:20 +0300

    HomeWork1 added

--- Lessons/01. Hello World. Data Types. Base Syntax/Homework/Task 2. ReverseString/index.js
+++ Lessons/01. Hello World. Data Types. Base Syntax/Homework/Task 2. ReverseString/index.js
@@ -1,6 +1,10 @@
 const reverseString = (str) => {
     // Начало
-
+    if (str.length <= 1){
+        return str;
+    } else {
+        return reverseString(str.slice(1) + str[0])
+    }
     // Конец
 };
 
--- Lessons/01. Hello World. Data Types. Base Syntax/Homework/Task 3. SumSquareDifference/index.js
+++ Lessons/01. Hello World. Data Types. Base Syntax/Homework/Task 3. SumSquareDifference/index.js
@@ -1,6 +1,18 @@
 const sumSquareDifference = (num) => {
     // Начало
-    
+    let sum = 0;
+    for (let i = 1; i <= num ; i++) {
+        sum+= i;
+    }
+    const squareOfSum = Math.pow(sum, 2);
+
+    let sumOfSquares =0;
+    for (let i = 0; i < num; i++) {
+        sumOfSquares = Math.pow(sumOfSquares + i, 2);
+    }
+    const difference = squareOfSum - sumOfSquares;
+    return difference;
+
     // Конец
 };
 
