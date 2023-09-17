b77fec389f896fff6e8530044f6f477633fafc2f
MelnikovaSofya <melsofa@mail.ru>
2023-09-17 09:48:17 +0300

    Made the last task

--- Lessons/02. Functions. Arrays. Objects. This/Tasks/Task 6. GenerateDifference/index.js
+++ Lessons/02. Functions. Arrays. Objects. This/Tasks/Task 6. GenerateDifference/index.js
@@ -6,7 +6,30 @@ const onlyUnique = (array) => [ ...new Set(array) ];
 
 const generateDifference = (objOne, objTwo) => {
     // Начало
+    const resObj = { ...objOne, ...objTwo };
+    const firstKeys = Object.keys(objOne);
+    const secondKeys = Object.keys(objTwo);
 
+    for (let i = 0; i < firstKeys.length; i++) {
+        const curKey = firstKeys[i];
+        if (objTwo.hasOwnProperty(curKey)) {
+            if (objOne[curKey] !== objTwo[curKey]) {
+                resObj[curKey] = "changed";
+            } else {
+                resObj[curKey] = "unchanged";
+            }
+        } else {
+            resObj[curKey] = "deleted";
+        }
+    }
+
+    for (let i = 0; i < secondKeys.length; i++) {
+        const key = secondKeys[i];
+        if (!objOne.hasOwnProperty(key)) {
+            resObj[key] = "added";
+        }
+    }
+    return resObj;
     // Конец
 };
 
