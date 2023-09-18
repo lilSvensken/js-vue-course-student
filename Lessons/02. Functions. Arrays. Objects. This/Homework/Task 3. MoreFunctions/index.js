b106df74c83af3c1997a666cbdf6b96aa82c09ed
galimovb <119049879+galimovb@users.noreply.github.com>
2023-09-18 20:50:52 +0300

    hw2

--- Lessons/02. Functions. Arrays. Objects. This/Homework/Task 3. MoreFunctions/index.js
+++ Lessons/02. Functions. Arrays. Objects. This/Homework/Task 3. MoreFunctions/index.js
@@ -1,7 +1,10 @@
 const moreFunction = (number) => {
-    // Начало
-
-    // Конец
+    return (arr) => {
+        for(let i = 0; i < arr.length; i++){
+            arr[i]= arr[i]/2
+        }
+        return arr;
+    }
 };
 
 export default moreFunction;
