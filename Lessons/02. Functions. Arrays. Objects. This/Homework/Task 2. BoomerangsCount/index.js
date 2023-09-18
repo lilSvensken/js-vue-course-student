3046af989e24b020d4301212af2e4d831a9e892c
KasimovDanil637 <kasimovdanil637@gmail.com>
2023-09-18 21:10:19 +0300

    add second_homework

--- Lessons/02. Functions. Arrays. Objects. This/Homework/Task 2. BoomerangsCount/index.js
+++ Lessons/02. Functions. Arrays. Objects. This/Homework/Task 2. BoomerangsCount/index.js
@@ -1,6 +1,10 @@
 const getBoomerangsCount = (numbers) => {
     // Начало
-
+    let count = 0;
+    for (let i = 2; i < numbers.length; i++){
+        if(numbers[i-2] === numbers[i] && numbers[i-2] !== numbers[i-1]) count += 1;
+    }
+    return count;
     // Конец
 };
 
--- Lessons/02. Functions. Arrays. Objects. This/Homework/Task 3. MoreFunctions/index.js
+++ Lessons/02. Functions. Arrays. Objects. This/Homework/Task 3. MoreFunctions/index.js
@@ -1,7 +1,10 @@
 const moreFunction = (number) => {
     // Начало
-
+    return (numbers) => {
+        for (let i = 0; i < numbers.length; i++)
+            numbers[i] = numbers[i] / number;
+        return numbers;
+    }
     // Конец
-};
-
+}
 export default moreFunction;
