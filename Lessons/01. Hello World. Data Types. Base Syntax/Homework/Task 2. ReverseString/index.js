ec0d825ec7b6941d08c1c9707ce7d95f83829dd8
Safina.Daliya <daliyasamsa@gmail.com>
2023-09-19 21:34:21 +0300

    1hw

--- Lessons/01. Hello World. Data Types. Base Syntax/Homework/Task 2. ReverseString/index.js
+++ Lessons/01. Hello World. Data Types. Base Syntax/Homework/Task 2. ReverseString/index.js
@@ -1,6 +1,10 @@
 const reverseString = (str) => {
     // Начало
-
+    let str2 = ''
+    for (let i = str.length; i >= 0 ; i--) {
+        str2 = str2 + str.slice(i, i + 1)
+    }
+    return str2
     // Конец
 };
 
--- Lessons/01. Hello World. Data Types. Base Syntax/Homework/Task 3. SumSquareDifference/index.js
+++ Lessons/01. Hello World. Data Types. Base Syntax/Homework/Task 3. SumSquareDifference/index.js
@@ -1,6 +1,13 @@
 const sumSquareDifference = (num) => {
     // Начало
-    
+    let sum = 0;
+    let qv = 0;
+    for (let i = 0; i <= num; i++) {
+        sum = sum + Math.pow(i, 2);
+        qv = qv + i;
+    };
+    return Math.pow(qv,2) - sum;
+
     // Конец
 };
 
