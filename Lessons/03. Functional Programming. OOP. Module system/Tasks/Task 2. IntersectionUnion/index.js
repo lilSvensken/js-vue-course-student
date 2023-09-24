6d0d3abc9ddb03e7c01baa7624b834bbf61266cd
elizaveta.shvedchikova <eli3avetta.shvedchikova@gmail.com>
2023-09-24 08:21:31 +0300

    Решение тасок темы 3

--- Lessons/03. Functional Programming. OOP. Module system/Tasks/Task 2. IntersectionUnion/index.js
+++ Lessons/03. Functional Programming. OOP. Module system/Tasks/Task 2. IntersectionUnion/index.js
@@ -1,7 +1,10 @@
 const getIntersectionUnion = (arrOne, arrTwo) => {
-    // Начало
-
-    // Конец
+	return {
+		intersection: arrOne.filter(item => {
+			return arrTwo.includes(item)
+		}),
+		union: [...new Set([...arrOne, ...arrTwo])]
+	}
 };
 
 export default getIntersectionUnion;
