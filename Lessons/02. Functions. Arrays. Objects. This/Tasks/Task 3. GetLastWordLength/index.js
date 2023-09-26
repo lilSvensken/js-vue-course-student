5891b4010c36aa00150141ed011e60c721714066
Elina <Elinanurgal@mail.ru>
2023-09-26 15:56:01 +0300

    added 3 task

--- Lessons/02. Functions. Arrays. Objects. This/Tasks/Task 3. GetLastWordLength/index.js
+++ Lessons/02. Functions. Arrays. Objects. This/Tasks/Task 3. GetLastWordLength/index.js
@@ -2,13 +2,6 @@ const getLastWordLength = (str) => {
     str = str.trim();
     let words = str.split(' ');
     return words[words.length - 1].length;
-    /*let len = 0;
-    for(let i = str.length; i > 0; i--){
-        while (str[i] !== ' '){
-            len++;
-        }
-    }
-    return len;*/
 };
 
 export default getLastWordLength;
