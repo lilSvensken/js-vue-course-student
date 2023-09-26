3a5417fb7a706161a9afb1ac3ada681745f083c3
galimovb <119049879+galimovb@users.noreply.github.com>
2023-09-26 11:36:41 +0300

    hw3

--- Lessons/03. Functional Programming. OOP. Module system/Homework/Task 2. CarBrands/src/carBrand.js
+++ Lessons/03. Functional Programming. OOP. Module system/Homework/Task 2. CarBrands/src/carBrand.js
@@ -1,7 +1,19 @@
-class CarBrand {
-    // Начало
+class CarBrand{
+    constructor(model, power, accelerationtoHundreed){
+        this.model = model;
+        this.power = power;
+        this.accelerationtoHundreed = accelerationtoHundreed;
+    }
 
-    // Конец
+    runDrug(time){
+        return "${this.model} runs drag for ${time} seconds"
+    }
+    getWhoIsFaster(car){
+        if(this.accelerationtoHundreed > car.accelerationtoHundreed){
+            return "${car.model} is faster for ${this.accelerationtoHundreed - car.accelerationtoHundreed} seconds"
+        }else{
+            return '${this.model} is faster for ${car.accelerationtoHundreed - this.accelerationtoHundreed} seconds'
+        }
+    }
 }
-
 export default CarBrand;
