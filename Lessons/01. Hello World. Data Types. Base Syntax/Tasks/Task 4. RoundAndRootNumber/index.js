23ff924761329dde8753f4dadad477f6f5e4259d
Impax666 <immortalyfleks@gmail.com>
2023-09-30 17:34:02 +0300

    lesson1 task4

--- Lessons/01. Hello World. Data Types. Base Syntax/Tasks/Task 4. RoundAndRootNumber/index.js
+++ Lessons/01. Hello World. Data Types. Base Syntax/Tasks/Task 4. RoundAndRootNumber/index.js
@@ -1,11 +1,8 @@
 const roundAndRootNumber = (num) => {
-    // Начало
-    let answer = Math.sqrt(num);
-    if (num < 0) return "error";
-    else {
-        return (String)(Math.round(answer * 100) / 100);
-        //toFixed(2);
-    }
-    // Конец
+
+    if (num < 0) return 'error'
+
+    return Math.floor(Math.sqrt(num) * 100 ) / 100 + ''
+
 };
 export default roundAndRootNumber;
