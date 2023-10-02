54ad87f0d56dc61e6610e9ae1e474d39b2516988
Ruzil Zakirov <ruzyzakov@gmail.com>
2023-10-02 16:28:14 +0300

    [add] Second HW

--- Lessons/02. Functions. Arrays. Objects. This/Homework/Task 2. BoomerangsCount/index.js
+++ Lessons/02. Functions. Arrays. Objects. This/Homework/Task 2. BoomerangsCount/index.js
@@ -1,7 +1,10 @@
 const getBoomerangsCount = (numbers) => {
-    // Начало
-
-    // Конец
+    let boomerangsCount = 0;
+    if (numbers.length < 3) return boomerangsCount;
+    for(let i = 0; i < numbers.length - 2; i++) {
+        if (numbers[i] === numbers[i+2] && numbers[i] != numbers[i+1]) boomerangsCount++;
+    }
+    return boomerangsCount;
 };
 
 export default getBoomerangsCount;
