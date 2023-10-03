9c741acf8376a57be66fd5b987884262a526e220
AmyArgotti <119971666+AmyArgotti@users.noreply.github.com>
2023-10-03 22:02:52 +0300

    task2

--- Lessons/02. Functions. Arrays. Objects. This/Homework/Task 1. IsSequenceContinuous/index.js
+++ Lessons/02. Functions. Arrays. Objects. This/Homework/Task 1. IsSequenceContinuous/index.js
@@ -1,7 +1,22 @@
 const isSequenceContinuous = (numbers) => {
-    // Начало
-
-    // Конец
+    let scr = true;
+    if ( num.length > 1 )
+    {
+    for(let a = 0; a < num.length - 1 ; a++){
+    if(num[a+1] - num[a]=== 1){
+      scr = true;
+    }
+    else
+    {
+      scr = false;
+      break;
+    }
+    }
+    }
+    else{
+    scr = false;
+    }
+    return scr;
 };
 
 export default isSequenceContinuous;
--- Lessons/02. Functions. Arrays. Objects. This/Homework/Task 2. BoomerangsCount/index.js
+++ Lessons/02. Functions. Arrays. Objects. This/Homework/Task 2. BoomerangsCount/index.js
@@ -1,6 +1,13 @@
 const getBoomerangsCount = (numbers) => {
     // Начало
-
+    let cont = 0;
+    for ( let a = 1; a< num.length; a++){
+       if ( num[a-1] === num[a+1] && num[a] !== num[a-1])
+       {
+       cont += 1;
+       }
+    }
+    return cont;
     // Конец
 };
 
