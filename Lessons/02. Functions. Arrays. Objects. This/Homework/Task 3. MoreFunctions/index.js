5034585799b31a2e0cbbf799d2732739635a2cdf
AmyArgotti <119971666+AmyArgotti@users.noreply.github.com>
2023-10-03 22:07:38 +0300

    task03

--- Lessons/02. Functions. Arrays. Objects. This/Homework/Task 3. MoreFunctions/index.js
+++ Lessons/02. Functions. Arrays. Objects. This/Homework/Task 3. MoreFunctions/index.js
@@ -1,6 +1,13 @@
 const moreFunction = (number) => {
     // Начало
-
+const mat = (ary) => {
+  let newAry = [];
+  for( let a = 0; i < ary.length; i++){
+  newAry[a] = ary[a] / num;
+  }
+  return newAry;
+}
+return mat;
     // Конец
 };
 
