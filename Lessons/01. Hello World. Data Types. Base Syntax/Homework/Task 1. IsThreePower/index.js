9b900f49bd61123f52435b503c8217edb6a3150b
AmyArgotti <119971666+AmyArgotti@users.noreply.github.com>
2023-10-03 23:13:48 +0300

    hmwrk

--- Lessons/01. Hello World. Data Types. Base Syntax/Homework/Task 1. IsThreePower/index.js
+++ Lessons/01. Hello World. Data Types. Base Syntax/Homework/Task 1. IsThreePower/index.js
@@ -1,6 +1,15 @@
 const isThreePower = (num) => {
     // Начало
-
+if ( n > 0) {
+for ( let a = 0; a<= Math.sqrt(n); a++){
+if (Math.pow ( 3, a) === n){
+return true;
+}
+}
+return false;
+}else {
+return false;
+}
     // Конец
 };
 
