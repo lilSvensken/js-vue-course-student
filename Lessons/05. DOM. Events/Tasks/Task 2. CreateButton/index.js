99502df3aece331b8ac1339a160e596f2b2709f8
alinakamalova204 <alinakamalova919@gmail.com>
2023-10-10 15:55:27 +0300

    Merge branch 'master' of https://github.com/lilSvensken/js-vue-course-student into kamalova

--- Lessons/05. DOM. Events/Tasks/Task 2. CreateButton/index.js
+++ Lessons/05. DOM. Events/Tasks/Task 2. CreateButton/index.js
@@ -1,5 +1,17 @@
 const createButton = () => {
     // Начало
+    const container = document.body.querySelector('.button-container');
+    const button = document.createElement('button');
+    button.textContent = 'Кнопка';
+    button.classList.add('btn','btn-primary');
+    container.append(button);
+
+    button.addEventListener('click',() => {
+        const description = document.querySelector('.description');
+        const  textSpan = document.createElement('span');
+        textSpan.textContent = 'Текст вставлен!'
+        description.appendChild(textSpan);
+    });
 
     // Конец
 };
