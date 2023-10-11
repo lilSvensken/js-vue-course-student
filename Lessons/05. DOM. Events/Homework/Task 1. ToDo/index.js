7982edfef4c2cab2a93792b5b3dac21e8f2f66d0
AltinRuslan <altinbaev.ruslan.2003@gmail.com>
2023-10-11 23:10:25 +0300

    added hw5

--- Lessons/05. DOM. Events/Homework/Task 1. ToDo/index.js
+++ Lessons/05. DOM. Events/Homework/Task 1. ToDo/index.js
@@ -1,6 +1,39 @@
 const setToDo = () => {
     // Начало
+    const main = document.querySelector('.to-do');
+    const tasks = document.querySelector('.to-do__tasks');
+    const input = document.querySelector('.to-do__main__input');
+    const button1 = document.querySelector('.to-do__main__button-planned');
+    const button2 = document.querySelector('.to-do__main__button-completed');
 
+
+    button1.addEventListener('click', () => {
+        const taskElement = document.createElement('span');
+        const taskText = input.value;
+        if (taskText === '') {
+            return;
+        }
+        taskElement.dataset.type = 'planned';
+        taskElement.textContent = taskText;
+        tasks.append(taskElement);
+        input.value = '';
+    });
+
+    button2.addEventListener('click', () => {
+        const taskElement = document.createElement('span');
+        const taskText = input.value;
+        if (taskText === '') {
+            return;
+        }
+        taskElement.dataset.type = 'completed';
+        taskElement.textContent = taskText;
+        tasks.append(taskElement);
+        input.value = '';
+    });
+
+    tasks.addEventListener('click', (event) => {
+        event.target.remove()
+    })
     // Конец
 };
 
--- Lessons/05. DOM. Events/Homework/Task 2. Calculator/index.js
+++ Lessons/05. DOM. Events/Homework/Task 2. Calculator/index.js
@@ -1,3 +1,5 @@
+// import {c} from "sinon/lib/sinon/spy-formatters";
+
 const TYPES = {
     DIGIT: 'digit',
     PLUS: 'plus',
@@ -21,7 +23,48 @@ const ADDITIONAL_BUTTONS = [
 ];
 
 const setCalculator = () => {
+
+    let a = '';
+
+    const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
     // Начало
+    const main = document.querySelector('.calc__main');
+    const out = document.querySelector('.calc__result-number');
+
+
+
+    document.querySelector('.calc__buttons').addEventListener('click', (event) => {
+        // нажата кнопка "Сбросить"
+
+
+        main.textContent = '';
+        const key = event.target.textContent;
+
+        if(digit.includes(key)){
+            a += key;
+            console.log(a)
+            main.textContent = a;
+        }
+
+        for (let i = 0; i < ADDITIONAL_BUTTONS.length; i++) {
+            if (ADDITIONAL_BUTTONS[i].text === key && key !== 'Сбросить' && key !== '=') {
+                a += ADDITIONAL_BUTTONS[i].text;
+                main.textContent = a;
+            } else if (ADDITIONAL_BUTTONS[i].text === key && key === 'Сбросить') {
+                a = '';
+                main.textContent = a;
+                out.textContent = '';
+            } else if (ADDITIONAL_BUTTONS[i].text === key && key === '=') {
+                main.textContent = a;
+                out.textContent = eval(a);
+
+            }
+        }
+    })
+
+    // КОД САМ ПО СЕБЕ РАБОТАЕТ, НО ДУМАЮ ПРОВЕРКУ ТЕСТОМ НЕ ПРОЙДЕТ, ПОТОМУ ЧТО
+    // РАБОТА КАЛЬКУЛЯТОРА БЫЛА ЧУТЬ ИЗМЕНЕНА
+
 
     // Конец
 };
