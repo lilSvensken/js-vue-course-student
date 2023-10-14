1d441aa8517260bc34c38e7c838c8d4fa7035a39
Julian <juli2003juli@gmail.com>
2023-10-14 16:30:14 +0300

    homework 5 was added

--- Lessons/05. DOM. Events/Homework/Task 1. ToDo/index.js
+++ Lessons/05. DOM. Events/Homework/Task 1. ToDo/index.js
@@ -1,6 +1,38 @@
 const setToDo = () => {
     // Начало
+    const toDO = document.body.querySelector('.to-do__main');
+    const taskContainer = document.body.querySelector('.to-do__tasks')
 
+    const info = document.createElement('input')
+    info.classList.add('form-control', 'form-control-lg');
+    info.type = 'text';
+    info.placeholder = 'Введитие наименвание задачи';
+
+    const buttonPlaned = document.createElement('button');
+    buttonPlaned.classList.add('btn', 'btn-danger');
+    buttonPlaned.textContent = 'Добавить в план';
+
+    const buttonCompleted = document.createElement('button');
+    buttonCompleted.classList.add('btn', 'btn-success');
+    buttonCompleted.textContent = 'Добавить в сделанные';
+
+    toDO.append(info, buttonPlaned, buttonCompleted);
+
+    const addListener = (element, type) => {
+        element.addEventListener('click', () => {
+        const elementType = document.createElement('span');
+        const text = info.value;
+        elementType.dataset.type = type;
+        elementType.textContent = text;
+            elementType.addEventListener('click', () => {
+                elementType.remove();
+            });
+        info.value = '';
+        taskContainer.append(elementType);
+        })
+    }
+    addListener(buttonPlaned, 'planned');
+    addListener(buttonCompleted, 'completed');
     // Конец
 };
 
--- Lessons/05. DOM. Events/Tasks/Task 1. Counter/index.js
+++ Lessons/05. DOM. Events/Tasks/Task 1. Counter/index.js
@@ -1,13 +1,16 @@
 const setCounter = () => {
     // Начало
-    const countText = 'Количество нажатий: ';
-    const counter = document.querySelector('.main');
-    const button = document.createElement('button');
-    button.classList.add('btn', 'btn-warning');
-    button.textContent = countText + '0';
+    const countText = 'Количество нажатий: '; //This const is just for the text of the button
+    const counter = document.querySelector('.main'); //We send all, that we do on the code
+    // with the const counter, to the html document;
+    const button = document.createElement('button'); //We just create the button
+    button.classList.add( 'btn', 'btn-warning'); // we design the button, the words between the labels are for appearance
+    //btn - the shape of the button, btn-warning - is the color
+    button.textContent = countText + '0'; //We add everything, that will be inside the button.
     
     button.addEventListener('click', (event) => {
-        const currentCount = event.target.textContent.split(': ')[1];
+        const currentCount = event.target.textContent.split(': ')[1]; //target is like a connector between methods
+        //we add all that is written in textContent and add "split and the numbers for the counter"
         const newCount = Number(currentCount) + 1;
         event.target.textContent = countText + newCount;
     });
