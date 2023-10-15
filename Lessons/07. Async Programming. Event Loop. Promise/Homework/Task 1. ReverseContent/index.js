7a86adf990b9ff3a5f647fc24267879aa363b886
Elina <Elinanurgal@mail.ru>
2023-10-15 21:48:08 +0300

    added task 1 from hw7

--- Lessons/07. Async Programming. Event Loop. Promise/Homework/Task 1. ReverseContent/index.js
+++ Lessons/07. Async Programming. Event Loop. Promise/Homework/Task 1. ReverseContent/index.js
@@ -1,17 +1,19 @@
-import { promises as fs } from 'fs';
+import {promises as fs, readFileSync} from 'fs';
 import path from 'path';
 
 
 // Метод для отладки. В итоговом решении использоваться не должен
 const getPath = (fileName) => path.join(__dirname, './__fixtures__', fileName);
 // Пример использования метода
-// const currentPath = getPath('/one.txt');
+//const currentPath = getPath('/one.txt');
 
 
 const reverseContent = (filepath) => {
-    // Начало
-
-    // Конец
+    let fileContent = fs.readFile(filepath, 'utf8');
+    fileContent.then(data => {
+        const reversedData = data.split('\n').reverse().join('\n');
+        return fs.writeFile(filepath, reversedData);
+    })
+    return fileContent;
 };
-
 export default reverseContent;
