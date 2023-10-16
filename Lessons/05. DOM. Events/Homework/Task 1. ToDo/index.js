ab3bdae72df05d9d16e94bf35eefdbceba03cf98
galimovb <119049879+galimovb@users.noreply.github.com>
2023-10-16 20:24:49 +0300

    hw5

--- Lessons/05. DOM. Events/Homework/Task 1. ToDo/index.js
+++ Lessons/05. DOM. Events/Homework/Task 1. ToDo/index.js
@@ -1,7 +1,39 @@
 const setToDo = () => {
-    // Начало
+    const main = document.querySelector(".to-do__main");
+    const tasksList = document.querySelector(".to-do__tasks");
 
-    // Конец
+    const input = document.createElement('input');
+    input.classList.add('form-control', 'form-control-lg');
+    input.type = 'text';
+    input.placeholder = 'Введите наименование задачи';
+    main.append(input);
+
+    const buttonPlanned = document.createElement('button');
+    buttonPlanned.classList.add('btn', 'btn-danger');
+    buttonPlanned.textContent = 'Добавить в план';
+
+    const buttonSuccess = document.createElement('button');
+    buttonSuccess.classList.add('btn', 'btn-success');
+    buttonSuccess.textContent = 'Добавить в сделанные';
+
+    const buttonListener = (element, type) => {
+        element.addEventListener('click', ()=>{
+            const taskElement = document.createElement('span');
+            taskElement.dataset.type = type;
+            taskElement.textContent = input.value;
+            taskElement.addEventListener('click', ()=>{
+                taskElement.remove();
+            });
+            input.value='';
+            tasksList.append(taskElement);
+        })
+    }
+    buttonListener(buttonPlanned, 'planned');
+    buttonListener(buttonSuccess, 'completed');
+
+    main.append(buttonPlanned);
+    main.append(buttonSuccess);
 };
+
 
 export default setToDo;
--- Lessons/05. DOM. Events/Homework/Task 2. Calculator/index.js
+++ Lessons/05. DOM. Events/Homework/Task 2. Calculator/index.js
@@ -1,29 +1,83 @@
+
 const TYPES = {
-    DIGIT: 'digit',
-    PLUS: 'plus',
-    RESULT: 'result',
-    RESET: 'reset',
+  DIGIT: 'digit',
+  PLUS: 'plus',
+  RESULT: 'result',
+  RESET: 'reset',
 };
 
 const ADDITIONAL_BUTTONS = [
-    {
-        text: '+',
-        type: TYPES.PLUS,
-    },
-    {
-        text: '=',
-        type: TYPES.RESULT,
-    },
-    {
-        text: 'Сбросить',
-        type: TYPES.RESET,
-    },
+  {
+    text: '+',
+    type: TYPES.PLUS,
+  },
+  {
+    text: '=',
+    type: TYPES.RESULT,
+  },
+  {
+    text: 'Сбросить',
+    type: TYPES.RESET,
+  },
 ];
 
 const setCalculator = () => {
-    // Начало
+  const textBlock = document.querySelector('.calc__main');
+  const result = document.querySelector(".calc__result");
+  const calc_buttons = document.querySelector(".calc__buttons");
+  const resultTextElement = document.createElement('span');
+  resultTextElement.textContent = 'Результат: ';
 
-    // Конец
+  const resultNumberElement = document.createElement('span');
+  resultNumberElement.classList.add('calc__result-number');
+  resultNumberElement.textContent = 0;
+  result.append(resultTextElement, resultNumberElement);
+
+  for (let i = 0; i <= 9; i++) {
+    const button = document.createElement('button');
+    button.classList.add('btn', 'btn-dark');
+    button.textContent = i.toString();
+    button.dataset.type = TYPES.DIGIT;
+    calc_buttons.append(button);
+  }
+
+  ADDITIONAL_BUTTONS.forEach((buttonData) => {
+    const button = document.createElement('button');
+    button.classList.add('btn', 'btn-dark');
+    button.textContent = buttonData.text;
+    button.dataset.type = buttonData.type;
+    calc_buttons.append(button);
+  });
+  const buttons_Array = document.querySelectorAll('button');
+  buttons_Array.forEach((button)=>{
+    const buttonType = button.dataset.type;
+    if(buttonType==='digit'){
+      button.addEventListener('click', ()=>{
+       const digit = button.textContent;
+        textBlock.textContent = textBlock.textContent + digit;
+      });
+    }else if(buttonType==='plus'){
+      button.addEventListener('click', ()=>{
+        textBlock.textContent = textBlock.textContent + '+';
+
+      });
+    }else if(buttonType==='result'){
+      button.addEventListener('click', ()=>{
+        const resultText =textBlock.textContent;
+        const sum = getsum(resultText);
+        resultNumberElement.textContent = sum;
+      });
+    } else if (buttonType === 'reset') {
+    button.addEventListener('click', (event) => {
+      const resultBlock = document.querySelector('.calc__result-number');
+      resultBlock.textContent = 0;
+      textBlock.textContent = '';
+    });
+  }
+
+  })
 };
-
+const getsum = (text) => text
+    .split('+')
+    .reduce((acc, num) => acc + Number(num), 0);
 export default setCalculator;
