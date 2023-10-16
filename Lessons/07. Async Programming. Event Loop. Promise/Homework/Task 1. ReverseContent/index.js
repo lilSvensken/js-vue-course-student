c9f124e2ee63c36bf1170a1fe2d7c01194c90583
Julian <juli2003juli@gmail.com>
2023-10-16 22:53:31 +0300

    homework 7 was added

--- Lessons/07. Async Programming. Event Loop. Promise/Homework/Task 1. ReverseContent/index.js
+++ Lessons/07. Async Programming. Event Loop. Promise/Homework/Task 1. ReverseContent/index.js
@@ -7,10 +7,24 @@ const getPath = (fileName) => path.join(__dirname, './__fixtures__', fileName);
 // Пример использования метода
 // const currentPath = getPath('/one.txt');
 
-
 const reverseContent = (filepath) => {
     // Начало
-
+    return new Promise((resolve, reject) =>{
+        fs.readFile(filepath, "utf-8")
+            .then((content) => {
+                const reversed = content
+                    .split('\n')
+                    .reverse()
+                    .join('\n');
+                return fs.writeFile(filepath, reversed, 'utf-8');
+            })
+            .then(() =>{
+                resolve();
+            })
+            .catch((error) => {
+                reject(error);
+        })
+    })
     // Конец
 };
 
--- Lessons/07. Async Programming. Event Loop. Promise/Tasks/Task 2. WriteSum Sync/index.js
+++ Lessons/07. Async Programming. Event Loop. Promise/Tasks/Task 2. WriteSum Sync/index.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 
 // Метод для отладки. В итоговом решении использоваться не должен
-const getPath = (fileName) => path.join(__dirname, './__fixtures__', fileName);
+// const getPath = (fileName) => path.join(__dirname, './__fixtures__', fileName);
 // Пример использования функции
 // const currentPath = getPath('/one.txt');
 
