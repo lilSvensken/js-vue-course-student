bd9328441be5cca96b6209b95eb8773004c9a374
Lazarev <egor0505-lazarev@mail.ru>
2023-10-16 18:42:20 +0300

    Task 2 modified

--- Lessons/01. Hello World. Data Types. Base Syntax/Homework/Task 2. ReverseString/index.js
+++ Lessons/01. Hello World. Data Types. Base Syntax/Homework/Task 2. ReverseString/index.js
@@ -1,7 +1,7 @@
 function reverseString(str)  {
     let rev = "";
     for (let i = str.length-1; i>=0; i--){
-        rev = rev + str.slice(i, i);
+        rev = rev + str.slice(i, i+1);
     }
     return rev;
 
