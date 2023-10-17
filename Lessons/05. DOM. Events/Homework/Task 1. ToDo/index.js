7217a4b3c5015a9c018d08bb45ae1bbcd958bb73
Сергей Трофимов <sm.trofimov2004@gmail.com>
2023-10-17 14:41:42 +0300

    homework

--- Lessons/05. DOM. Events/Homework/Task 1. ToDo/index.js
+++ Lessons/05. DOM. Events/Homework/Task 1. ToDo/index.js
@@ -1,7 +1,31 @@
 const setToDo = () => {
-    // Начало
+    const planBtn = document.getElementById("plan")
+    const successBtn = document.getElementById("success")
+    const tasksSpace = document.querySelector(".to-do__tasks")
+    const input = document.getElementById('text')
+    planBtn.addEventListener('click', () => {
+        var item = document.createElement('span')
+        item.innerHTML = input.value
+        input.value = ''
+        item.setAttribute('data-type', 'planned')
+        tasksSpace.append(item)
+        item.addEventListener('click', () => {
+            const parent = item.parentNode
+            parent.removeChild(item)
+        })
+    })
 
-    // Конец
+    successBtn.addEventListener('click', () => {
+        var item = document.createElement('span')
+        item.innerHTML = input.value
+        input.value = ''
+        item.setAttribute('data-type', 'completed')
+        tasksSpace.append(item)
+        item.addEventListener('click', () => {
+            const parent = item.parentNode
+            parent.removeChild(item)
+        })
+    })
 };
 
 export default setToDo;
