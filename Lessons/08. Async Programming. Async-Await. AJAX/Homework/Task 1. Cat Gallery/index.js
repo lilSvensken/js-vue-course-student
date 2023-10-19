93deafea7595ff5b383300ca926b5806cdb811b7
L1llong <longma2003@mail.ru>
2023-10-19 20:31:23 +0300

    08. HomeWork

--- Lessons/08. Async Programming. Async-Await. AJAX/Homework/Task 1. Cat Gallery/index.js
+++ Lessons/08. Async Programming. Async-Await. AJAX/Homework/Task 1. Cat Gallery/index.js
@@ -6,10 +6,38 @@
 
 // import axios from 'axios';
 
-const setCatGallery = () => {
-    // Начало
 
+
+ const setCatGallery = () => {
+    // Начало
+    const container = document.querySelector('.main__container')
+    const url = 'https://api.thecatapi.com/v1/images/search'
+
+    return new Promise((resolve, reject) => {
+        const requestArray = []
+
+        for (let i = 0; i < 10; i++) {
+            requestArray.push(fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    let dataUrl = data[0].url
+                    const img = document.createElement('img')
+                    img.src = dataUrl
+                    container.appendChild(img)
+                })
+                .catch(err => reject(err))
+            )
+        }
+
+        Promise.all(requestArray)
+            .then(() => resolve('cat gallery is ready!'))
+            .catch(err => reject(err))
+    })
     // Конец
 };
+
+// setCatGallery().then((text) => {
+//     console.log(text); // cat gallery is ready!
+// })
 
 export default setCatGallery;
