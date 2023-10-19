577b5ee96e3edd83a2f5f5a7c22c7b2c093cbb21
Adelygaynullova1 <adelya_gaynullova@mail.ru>
2023-10-19 18:35:40 +0300

    add homeWork 5

--- Lessons/05. DOM. Events/Homework/Task 1. ToDo/index.js
+++ Lessons/05. DOM. Events/Homework/Task 1. ToDo/index.js
@@ -1,7 +1,41 @@
 const setToDo = () => {
-    // Начало
+    const main = document.querySelector('.to-do__main');
+    const taskStorage = document.querySelector('.to-do__tasks');
 
-    // Конец
+    const inputPlace = document.createElement('input');
+    inputPlace.classList.add("form-control", "form-control-lg");
+    inputPlace.type = "text";
+    inputPlace.placeholder = "Введите наименование задачи";
+    main.append(inputPlace);
+
+    function adder (element, type){
+        element.addEventListener('click', () => {
+            const elementOfTask = document.createElement('span');
+            const dataOfTask = inputPlace.value;
+            elementOfTask.dataset.type = type;
+            elementOfTask.textContent = dataOfTask;
+            elementOfTask.addEventListener('click', () => {
+                elementOfTask.remove();
+            });
+            taskStorage.append(elementOfTask);
+            inputPlace.value = null;
+
+        });
+    }
+
+    const buttonPlanned = document.createElement('button');
+    buttonPlanned.classList.add('btn', 'btn-danger');
+    buttonPlanned.textContent = 'Добавить в план';
+
+    const buttonCompleted = document.createElement('button');
+    buttonCompleted.classList.add('btn', 'btn-success');
+    buttonCompleted.textContent = 'Добавить в сделанные';
+
+    adder(buttonPlanned, 'planned');
+    adder(buttonCompleted, 'completed');
+
+    main.append(buttonPlanned);
+    main.append(buttonCompleted);
 };
 
 export default setToDo;
--- Lessons/05. DOM. Events/Homework/Task 2. Calculator/index.js
+++ Lessons/05. DOM. Events/Homework/Task 2. Calculator/index.js
@@ -20,10 +20,74 @@ const ADDITIONAL_BUTTONS = [
     },
 ];
 
-const setCalculator = () => {
-    // Начало
 
-    // Конец
+const getSumOfInput = (text) => text
+    .split('+')
+    .reduce((sum, num) => sum + Number(num), 0);
+
+
+const setCalculator = () => {
+    const buttons = document.querySelector('.calc__buttons')
+    for (let i = 0; i<=9; i++) {
+        const button = document.createElement('button')
+        button.classList.add('btn', 'btn-dark');
+        button.dataset.type = TYPES.DIGIT
+        button.textContent = i
+        buttons.append(button)
+    }
+    ADDITIONAL_BUTTONS.forEach((sign) => {
+        const button = document.createElement('button')
+        button.classList.add('btn', 'btn-dark');
+        button.dataset.type = sign.type
+        button.textContent = sign.text
+        buttons.append(button)
+    })
+
+    const result = document.querySelector('.calc__result')
+    const textResult = document.createElement('span')
+    textResult.textContent = 'Резултат:'
+    const countOfResult = document.createElement('span')
+    countOfResult.classList.add('calc__result-number')
+    countOfResult.textContent = 0
+    result.append(textResult,countOfResult)
+
+    const setOfSign = document.querySelector('.calc__main')
+
+    const allButtons = buttons.querySelectorAll('button');
+    [ ...allButtons ].forEach((button) => {
+            const typeOfButton = button.dataset.type
+
+        if (typeOfButton === TYPES.DIGIT) {
+            button.addEventListener('click', (event) => {
+                setOfSign.textContent = setOfSign.textContent + button.textContent
+            });
+
+        }
+        else if (typeOfButton === TYPES.PLUS) {
+            button.addEventListener('click', (event) => {
+                setOfSign.textContent = setOfSign.textContent + '+'
+            });
+
+        }
+        else if (typeOfButton === TYPES.RESULT) {
+            button.addEventListener('click', (event) => {
+                const dataOfResult = setOfSign.textContent
+                const sumOfAll = getSumOfInput(dataOfResult)
+                const result = document.querySelector('.calc__result-number')
+                result.textContent = sumOfAll
+            });
+
+        }
+        else if (typeOfButton === TYPES.RESET) {
+            button.addEventListener('click', (event) => {
+                const result = document.querySelector('.calc__result-number')
+                setOfSign.textContent = null
+                result.textContent = 0
+            });
+        }
+    })
+
+
 };
 
 export default setCalculator;
