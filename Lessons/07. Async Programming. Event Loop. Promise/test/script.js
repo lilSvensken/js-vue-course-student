3a007c90736e32283700367dd12e9d530c243002
Julian <juli2003juli@gmail.com>
2023-10-22 17:34:25 +0300

    homework 8 was added

--- Lessons/07. Async Programming. Event Loop. Promise/test/script.js
+++ Lessons/07. Async Programming. Event Loop. Promise/test/script.js
@@ -1,34 +1,54 @@
-const selectRef = document.getElementById('select');
-const loadRef = document.getElementById("load")
-setDefaultColor();
-onListener();
+const getTodos = fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify({
+        userId: 1,
+        title: 'foo',
+        body: 'bar'
 
-function onListener () {
-  const promise = new Promise((resolve, reject) => {
-    selectRef.addEventListener('change', () => {
-        const value = selectRef.value;
-        localStorage.setItem("COLOR_LS", value);
-        
-        if (value === 'red') {
-          reject("Ошибка, красный")
-        } else if (value === 'green') {
-          resolve("Ок, зеленый")
-        }
-      }
-    )
-  })
-  
-  promise
-    .then((data) => console.log(data))
-    .catch((err) => console.log(err))
-    .finally(() => onListener())
-}
+    }),
+    headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+    },
+})
+getTodos
+    .then((data) => data.json())
+    .then((data) => {
+        console.log(data)
+        document.body.innerText = data.title
+    })
+    .catch((err) => console.error(err))
 
-function setDefaultColor () {
-  selectRef.value = localStorage.getItem("COLOR_LS");
-  
-  setTimeout(() => {
-    loadRef.classList.add("hide");
-    selectRef.classList.remove("hide");
-  }, 2000)
-}
+// const selectRef = document.getElementById('select');
+// const loadRef = document.getElementById("load")
+// setDefaultColor();
+// onListener();
+//
+// function onListener () {
+//   const promise = new Promise((resolve, reject) => {
+//     selectRef.addEventListener('change', () => {
+//         const value = selectRef.value;
+//         localStorage.setItem("COLOR_LS", value);
+//
+//         if (value === 'red') {
+//           reject("Ошибка, красный")
+//         } else if (value === 'green') {
+//           resolve("Ок, зеленый")
+//         }
+//       }
+//     )
+//   })
+//
+//   promise
+//     .then((data) => console.log(data))
+//     .catch((err) => console.log(err))
+//     .finally(() => onListener())
+// }
+//
+// function setDefaultColor () {
+//   selectRef.value = localStorage.getItem("COLOR_LS");
+//
+//   setTimeout(() => {
+//     loadRef.classList.add("hide");
+//     selectRef.classList.remove("hide");
+//   }, 2000)
+// }
--- Lessons/08. Async Programming. Async-Await. AJAX/Homework/Task 1. Cat Gallery/index.js
+++ Lessons/08. Async Programming. Async-Await. AJAX/Homework/Task 1. Cat Gallery/index.js
@@ -4,12 +4,35 @@
 // Выполните задание, отслеживая изменения в браузере,
 // после чего раскомментируйте строку с импортом для прохождения тестов
 
-// import axios from 'axios';
-
-const setCatGallery = () => {
+import axios from 'axios';
+const url = 'https://api.thecatapi.com/v1/images/search';
+async function setCatGallery(){
     // Начало
+    const catImageURLS = [];
+    const requests = Array.from({length: 10}, async () => {
+     try {
+      const response = await axios.get(url);
+      const catData = response.data[0];
+      if (catData && catData.url) {
+       catImageURLS.push(catData.url)
+      }
+     } catch (error) {
+      throw error;
+     }
+    });
+    await Promise.all(requests);
+    const mainContainer = document.querySelector('.main__container');
+    catImageURLS.forEach(catImageUrl => {
+     const imgElement = document.createElement('img');
+     imgElement.src = catImageUrl;
+     mainContainer.appendChild(imgElement);
+    });
 
+    return 'cat gallery is ready!';
     // Конец
-};
+}
+setCatGallery()
+    .then(message => console.log(message))
+    .catch(error => console.error(error));
 
 export default setCatGallery;
