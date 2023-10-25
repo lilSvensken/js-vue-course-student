dd32d3c10ed13cbb4a0e34eff6d0d30953e8dada
Impax666 <immortalyfleks@gmail.com>
2023-10-25 14:05:51 +0300

    little fix

--- Lessons/09. Tests. Date. URL. Linting. Storages/Homework/Task 2. refactorMe/index.js
+++ Lessons/09. Tests. Date. URL. Linting. Storages/Homework/Task 2. refactorMe/index.js
@@ -1,24 +1,19 @@
 const getTextFromUrl = (url) => {
-    const textReplacements = {
-        'text': ' ',
-        'coma': ',',
-        'dot': '.',
-        'space': ' ',
-        'semicolon': ';',
-        'apostrophe': "'",
-        '&': '',
-        '=': '',
-    };
-
-    const decodeText = (text) => {
-        for (const [key, value] of Object.entries(textReplacements)) {
-            text = text.replaceAll(key, value);
-        }
-        return text;
-    };
-
-    return decodeText(new URL(url).searchParams.toString());
+    // Начало
+    const o = new URL(url);
+    var par = o.searchParams;
+    var str = par.toString();
+    let rep_text = str.replaceAll('text=', ' ');
+    let rep_c = rep_text.replaceAll('coma', ',');
+    let rep_d = rep_c.replaceAll('dot', '.');
+    let space = rep_d.replaceAll('space', ' ');
+    let sc = space.replaceAll('semicolon', ';');
+    var apth = sc.replaceAll('apostrophe', "'");
+    var ampRepl = apth.replaceAll('&', '');
+    var ravno = ampRepl.replaceAll('=', '');
+    var deleteProbel = ravno.replaceAll("' ", "'");
+    return deleteProbel;
+    // Конец
 };
+
 export default getTextFromUrl;
-
-
