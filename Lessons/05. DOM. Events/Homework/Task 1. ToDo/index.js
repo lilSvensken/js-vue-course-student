960112b50fbcdc609fd6ce6b745f184aa70c4c80
Lazarev <egor0505-lazarev@mail.ru>
2023-10-25 19:12:13 +0300

    Homework 5 added

--- Lessons/05. DOM. Events/Homework/Task 1. ToDo/index.js
+++ Lessons/05. DOM. Events/Homework/Task 1. ToDo/index.js
@@ -1,7 +1,44 @@
 const setToDo = () => {
-    // Начало
-
-    // Конец
+    const toDoMain = document.querySelector('.to-do__main');
+    const toDoMainInputText = document.createElement('input');
+    toDoMainInputText.type = 'text';
+    toDoMainInputText.classList.add('form-control', 'form-control-lg');
+    const btnDanger = document.createElement('button');
+    btnDanger.classList.add('btn', 'btn-danger');
+    btnDanger.textContent = 'Добавить в план';
+    const btnSuccess = document.createElement('button');
+    btnSuccess.classList.add('btn', 'btn-success');
+    btnSuccess.textContent = 'Добавить в сделанные';
+    toDoMain.append(toDoMainInputText, btnDanger, btnSuccess);
+    const tasks = document.querySelector('.to-do__tasks');
+    btnDanger.addEventListener('click', (event) => {
+        if (toDoMainInputText.value === '') {
+            alert('Ничего не написано');
+        } else {
+            let span = document.createElement('span');
+            span.appendChild(document.createTextNode(toDoMainInputText.value));
+            span.dataset.type = 'planned';
+            span.addEventListener('click', (event) => {
+                span.remove();
+            })
+            tasks.appendChild(span);
+            toDoMainInputText.value = '';
+        }
+    })
+    btnSuccess.addEventListener('click', (event) => {
+        if (toDoMainInputText.value === '') {
+            alert('Ничего не написано');
+        }else {
+            let span = document.createElement('span');
+            span.appendChild(document.createTextNode(toDoMainInputText.value));
+            span.dataset.type = 'completed';
+            span.addEventListener('click', (event) => {
+            span.remove();
+        })
+            tasks.appendChild(span);
+            toDoMainInputText.value = '';
+        }
+    })
 };
 
 export default setToDo;
--- Lessons/05. DOM. Events/Homework/Task 2. Calculator/index.js
+++ Lessons/05. DOM. Events/Homework/Task 2. Calculator/index.js
@@ -21,9 +21,46 @@ const ADDITIONAL_BUTTONS = [
 ];
 
 const setCalculator = () => {
-    // Начало
+    const buttons = document.querySelector('.calc__buttons');
+    const main = document.querySelector('.calc__main');
+    const result = document.querySelector('.calc__result');
+    const btnValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, '+', '=', 'Сбросить'];
+    for (let  i of btnValues){
+        let btn = document.createElement('button');
+        btn.classList.add('btn', 'btn-dark');
+        btn.textContent = i;
+        if (i !== '=' && i!== 'Сбросить'){
+            btn.addEventListener('click', (event) => {
+                main.textContent = main.textContent.concat(btn.textContent);
+            })
+        }if (i === '='){
+            btn.id = 'result';
+        }if (i === 'Сбросить'){
+            btn.id = 'clear';
+        }
+        buttons.append(btn);
+    }
+    const  resultText = document.createElement('span');
+    resultText.textContent = 'Результат:';
+    let resultNumber = document.createElement('span');
+    resultNumber.classList.add('calc__result-number');
+    resultNumber.textContent = '0';
+    result.append(resultText, resultNumber);
+    const clear = document.getElementById('clear');
+    clear.addEventListener('click', (event) =>{
+        resultNumber.textContent = '0';
+        main.textContent ='';
+    })
+    const res = document.getElementById('result');
 
-    // Конец
+    res.addEventListener('click', (event) =>{
+        let equation = main.textContent.split('+');
+        let r = 0;
+        for (let i of equation){
+            r = r + Number(i);
+        }
+        resultNumber.textContent = r;
+    })
 };
 
 export default setCalculator;
