c1c3a5d5056a8c884bbb605db9dd52cbbbd65136
AmyArgotti <119971666+AmyArgotti@users.noreply.github.com>
2023-10-25 19:45:12 +0300

    lesson 5

--- Lessons/05. DOM. Events/Tasks/Task 1. Counter/index.js
+++ Lessons/05. DOM. Events/Tasks/Task 1. Counter/index.js
@@ -1,6 +1,18 @@
 const setCounter = () => {
     // Начало
+    const countText = 'Number of clicks: ';
+    const counter = document.querySelector('.main');
+    const button = document.createElement('button');
+    button.classList.add('btn', 'btn-warning');
+    button.textContent = countText + '0';
 
+    button.addEventListener('click', (event) => {
+        const currentCount = event.target.textContent.split(': ')[1];
+        const newCount = Number(currentCount) + 1;
+        event.target.textContent = countText + newCount;
+    });
+
+    counter.append(button);
     // Конец
 };
 
--- Lessons/05. DOM. Events/Tasks/Task 2. CreateButton/index.js
+++ Lessons/05. DOM. Events/Tasks/Task 2. CreateButton/index.js
@@ -1,6 +1,16 @@
 const createButton = () => {
     // Начало
-
+    const container = document.body.querySelector('.button-container');
+    const button = document.createElement('button');
+    button.classList.add('btn', 'btn-primary');
+    button.textContent = 'the coolest button';
+    container.append(button);
+    button.addEventListener('click', () => {
+        const description = document.querySelector('.description');
+        const textSpan = document.createElement('span');
+        textSpan.textContent = 'you are cute!';
+        description.append(textSpan);
+    });
     // Конец
 };
 
