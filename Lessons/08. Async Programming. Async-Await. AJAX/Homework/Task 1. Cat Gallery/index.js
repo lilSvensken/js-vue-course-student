0fb32bf1e27a37fa5c2bc8ababea9a492e2392f1
Lazarev <egor0505-lazarev@mail.ru>
2023-10-27 00:21:29 +0300

    Homework 8 added

--- Lessons/08. Async Programming. Async-Await. AJAX/Homework/Task 1. Cat Gallery/index.js
+++ Lessons/08. Async Programming. Async-Await. AJAX/Homework/Task 1. Cat Gallery/index.js
@@ -3,13 +3,29 @@
 // <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 // Выполните задание, отслеживая изменения в браузере,
 // после чего раскомментируйте строку с импортом для прохождения тестов
-
+import fetch from "node-fetch";
 // import axios from 'axios';
 
 const setCatGallery = () => {
-    // Начало
+    const container = document.querySelector('.main__container');
+    const catUrl = 'https://api.thecatapi.com/v1/images/search';
+    const getAndInsertCatPicture = async () => {
+        let response = await fetch(catUrl);
+        let result = await response.json();
+        return result[0];
 
-    // Конец
+    }
+
+    let promis = Promise.all([getAndInsertCatPicture(), getAndInsertCatPicture(), getAndInsertCatPicture(), getAndInsertCatPicture(), getAndInsertCatPicture(), getAndInsertCatPicture(), getAndInsertCatPicture(), getAndInsertCatPicture(), getAndInsertCatPicture(), getAndInsertCatPicture()]);
+    let p = promis.then((result1) => {
+        for (let i of result1){
+            let img = document.createElement('img');
+            img.src = i.url;
+            container.append(img);
+        }
+        return 'cat gallery is ready!';
+    });
+    return p;
 };
 
 export default setCatGallery;
