c1c829ae51b425b43abb2fa284e0312b6a845ac5
MelnikovaSofya <melsofa@mail.ru>
2023-10-29 20:06:07 +0300

    Made hw 8

--- Lessons/08. Async Programming. Async-Await. AJAX/Homework/Task 1. Cat Gallery/index.js
+++ Lessons/08. Async Programming. Async-Await. AJAX/Homework/Task 1. Cat Gallery/index.js
@@ -4,10 +4,30 @@
 // Выполните задание, отслеживая изменения в браузере,
 // после чего раскомментируйте строку с импортом для прохождения тестов
 
-// import axios from 'axios';
+import axios from 'axios';
 
 const setCatGallery = () => {
     // Начало
+    const apiUrl = 'https://api.thecatapi.com/v1/images/search';
+    const numberOfRequests = 10;
+    const requests = Array.from({ length: numberOfRequests }, () => axios.get(apiUrl));
+
+    return Promise.all(requests)
+        .then((responses) => {
+            const imagesUrls = responses.map((response) => response.data[0].url);
+            const container = document.querySelector('.main__container');
+
+            imagesUrls.forEach((imageUrl) => {
+                const imgElement = document.createElement('img');
+                imgElement.src = imageUrl;
+                container.appendChild(imgElement);
+            });
+
+            return 'cat gallery is ready!';
+        })
+        .catch((error) => {
+            console.error(error);
+        });
 
     // Конец
 };
