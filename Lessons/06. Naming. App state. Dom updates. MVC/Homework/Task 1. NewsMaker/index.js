f109d76744d3d9d02cbb7e2072cc1c47b0cca6f9
Aleksandra <alexi.rr@mail.ru>
2023-10-31 00:23:11 +0300

    Sixth homework

--- Lessons/06. Naming. App state. Dom updates. MVC/Homework/Task 1. NewsMaker/index.js
+++ Lessons/06. Naming. App state. Dom updates. MVC/Homework/Task 1. NewsMaker/index.js
@@ -1,10 +1,47 @@
-import WatchJS from '../../helpers/melanke-watchjs.js';
-
-const { watch } = WatchJS;
+import * as WatchJS from '../../helpers/melanke-watchjs.js';
 
 const setNewsMaker = () => {
-    // Начало
+    const addNewsButton = document.querySelector("button");
+    const newsInput = document.querySelector('input');
+    const newsContainer = document.querySelector(".news__container")
 
+    // Начало
+    const state = {
+        latestNews: {
+            text: "",
+            date: ""
+        }
+    };
+
+    function watchState(state){
+        watch(state, 'text', () => {
+            if(state.text === ''){
+                newsInput.value = state.text
+            }else{
+                const card = document.createElement("div")
+                card.classList.add("news__news-element")
+
+                const cardDate = document.createElement("h5")
+                cardDate.textContent = state.date
+
+                const cardText = document.createElement("div")
+                cardText.textContent = state.text
+
+                card.append(cardDate, cardText)
+
+                newsContainer.prepend(card)
+                state.text = ""
+                state.date = ""
+            }
+        })
+    }
+
+    addNewsButton.addEventListener('click', () => {
+        state.text = newsInput.value;
+        state.date = new Date().toLocaleString();
+    });
+
+    watchState(state)
     // Конец
 };
 
