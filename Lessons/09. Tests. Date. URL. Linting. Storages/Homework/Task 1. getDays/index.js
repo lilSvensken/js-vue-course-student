0318cae93cc1769be2933110857298720b0f7891
KasimovDanil637 <kasimovdanil637@gmail.com>
2023-10-30 20:11:41 +0300

    add homework_9

--- Lessons/09. Tests. Date. URL. Linting. Storages/Homework/Task 1. getDays/index.js
+++ Lessons/09. Tests. Date. URL. Linting. Storages/Homework/Task 1. getDays/index.js
@@ -1,7 +1,25 @@
-const getDaysAmount = (month, year) => {
-    // Начало
-
-    // Конец
+const monthNames = {
+    January: 0,
+    February: 1,
+    March: 2,
+    April: 3,
+    May: 4,
+    June: 5,
+    July: 6,
+    August: 7,
+    September: 8,
+    October: 9,
+    November: 10,
+    December: 11
 };
+function getDaysAmount(month, year) {
+    let numberMonth = monthNames[month]
+    if (numberMonth === undefined) {
+        return 'error';
+    }
+    const date = new Date(year, numberMonth + 1, 1);
+    date.setDate(date.getDate() - 1)
+    return date.getDate();
 
-export default getDaysAmount;
+}
+ export default getDaysAmount;
--- Lessons/09. Tests. Date. URL. Linting. Storages/Homework/Task 2. refactorMe/index.js
+++ Lessons/09. Tests. Date. URL. Linting. Storages/Homework/Task 2. refactorMe/index.js
@@ -1,19 +1,24 @@
 const getTextFromUrl = (url) => {
-    // Начало
-    const o = new URL(url);
-    var par = o.searchParams;
-    var str = par.toString();
-    let rep_text = str.replaceAll('text=', ' ');
-    let rep_c = rep_text.replaceAll('coma', ',');
-    let rep_d = rep_c.replaceAll('dot', '.');
-    let space = rep_d.replaceAll('space', ' ');
-    let sc = space.replaceAll('semicolon', ';');
-    var apth = sc.replaceAll('apostrophe', "'");
-    var ampRepl = apth.replaceAll('&', '');
-    var ravno = ampRepl.replaceAll('=', '');
-    var deleteProbel = ravno.replaceAll("' ", "'");
-    return deleteProbel;
-    // Конец
+    const replacements = [
+        { key: 'text=', value: ' ' },
+        { key: 'coma', value: ',' },
+        { key: 'dot', value: '.' },
+        { key: 'space', value: ' ' },
+        { key: 'semicolon', value: ';' },
+        { key: 'apostrophe', value: "'" },
+        { key: '&', value: '' },
+        { key: '=', value: '' },
+        { key: "' ", value: "'" }
+    ];
+    const parsedUrl = new URL(url);
+    const searchParams = parsedUrl.searchParams;
+    let textSong = searchParams.toString();
+    replacements.forEach(({ key, value }) => {
+        textSong = textSong.replaceAll(key, value);
+    });
+
+    return textSong;
 };
+
 
 export default getTextFromUrl;
