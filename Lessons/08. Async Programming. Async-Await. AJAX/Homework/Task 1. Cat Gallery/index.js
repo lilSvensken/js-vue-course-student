72446ea026a3eff753cd6e7f941c6848b8a40205
KasimovDanil637 <kasimovdanil637@gmail.com>
2023-10-30 20:11:23 +0300

    add homework_8

--- Lessons/08. Async Programming. Async-Await. AJAX/Homework/Task 1. Cat Gallery/index.js
+++ Lessons/08. Async Programming. Async-Await. AJAX/Homework/Task 1. Cat Gallery/index.js
@@ -4,11 +4,28 @@
 // Выполните задание, отслеживая изменения в браузере,
 // после чего раскомментируйте строку с импортом для прохождения тестов
 
-// import axios from 'axios';
+import axios from 'axios';
 
+const BASE_URL = "https://api.thecatapi.com/v1/images/search"
 const setCatGallery = () => {
     // Начало
-
+    const mainContainer = document.querySelector('.main__container')
+    let picturesCats = []
+    for (let i = 0; i < 10; i++){
+        picturesCats[i] = (axios.get(BASE_URL))
+    }
+    return Promise.all(picturesCats)
+        .then((data) => {
+            data.forEach((object) => {
+                const img = document.createElement('img')
+                img.src = object.data[0].url
+                mainContainer.append(img)
+            })
+            return `cat gallery is ready!`
+        })
+        .catch((error) => {
+            console.log(error)
+        })
     // Конец
 };
 
