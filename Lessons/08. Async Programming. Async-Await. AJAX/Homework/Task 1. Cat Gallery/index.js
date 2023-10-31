a681d8115a7db458b32763dc428a3a6f10743ab6
alinakamalova204 <alinakamalova919@gmail.com>
2023-10-31 13:36:54 +0300

    Home Work8

--- Lessons/08. Async Programming. Async-Await. AJAX/Homework/Task 1. Cat Gallery/index.js
+++ Lessons/08. Async Programming. Async-Await. AJAX/Homework/Task 1. Cat Gallery/index.js
@@ -8,7 +8,27 @@
 
 const setCatGallery = () => {
     // Начало
+    const container = document.querySelector('.main__container');
 
+    // Функция для отправки запроса и добавления картинки на страницу
+    const fetchCatImage = () => {
+        return axios.get('https://api.thecatapi.com/v1/images/search')
+            .then((response) => {
+                const imageUrl = response.data[0].url;
+                const catImage = document.createElement('img');
+                catImage.src = imageUrl;
+                container.appendChild(catImage);
+            });
+    };
+
+    // Создаем массив промисов для 10 запросов
+    const promises = Array.from({ length: 10 }, () => fetchCatImage());
+
+    // Используем Promise.all для ожидания завершения всех запросов
+    return Promise.all(promises)
+        .then(() => {
+            return 'cat gallery is ready!';
+        });
     // Конец
 };
 
