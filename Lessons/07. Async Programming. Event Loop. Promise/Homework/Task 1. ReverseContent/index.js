12296e0ba3c441f4fefcef3b0bc890dd7c8afdeb
alinakamalova204 <alinakamalova919@gmail.com>
2023-10-31 13:16:20 +0300

    Home Work7

--- Lessons/07. Async Programming. Event Loop. Promise/Homework/Task 1. ReverseContent/index.js
+++ Lessons/07. Async Programming. Event Loop. Promise/Homework/Task 1. ReverseContent/index.js
@@ -7,11 +7,28 @@ const getPath = (fileName) => path.join(__dirname, './__fixtures__', fileName);
 // Пример использования метода
 // const currentPath = getPath('/one.txt');
 
-
+const fs = require('fs').promises;
+const readFile = (filePath => {
+    return fs.readFile(filePath,'utf8');
+});
+const writeFile = (filePath,content) => {
+    return fs.writeFile(filePath,content,'utf8');
+};
 const reverseContent = (filepath) => {
     // Начало
-
+    return readFile(filepath)
+        .then((content) => {
+            const reverseContent = content.split('\n').reverse().join('\n');
+            return writeFile(filepath,reverseContent);
+        });
     // Конец
 };
 
-export default reverseContent;
+// export default reverseContent;
+reverseContent('./__fixtures__/one.txt')
+    .then(() => {
+        console.log('Файл успешно перевернут.');
+    })
+    .catch((error) => {
+        console.error('Произошла ошибка:', error);
+    });
