5a3fe8817ba7446595ee68d3e6a6b9a0120d7795
Adelygaynullova1 <adelya_gaynullova@mail.ru>
2023-10-31 13:53:50 +0300

    add homeWork 9.1

--- Lessons/09. Tests. Date. URL. Linting. Storages/Homework/Task 1. getDays/index.js
+++ Lessons/09. Tests. Date. URL. Linting. Storages/Homework/Task 1. getDays/index.js
@@ -1,7 +1,27 @@
 const getDaysAmount = (month, year) => {
-    // Начало
+    const monthNames = {
+        January: 0,
+        February: 1,
+        March: 2,
+        April: 3,
+        May: 4,
+        June: 5,
+        July: 6,
+        August: 7,
+        September: 8,
+        October: 9,
+        November: 10,
+        December: 11
+    };
 
-    // Конец
+    const monthIndex = monthNames[month];
+
+    if (monthIndex === undefined) {
+        return 'error';
+    }
+
+    const date = new Date(year, monthIndex + 1, 0);
+
+    return date.getDate();
 };
-
 export default getDaysAmount;
