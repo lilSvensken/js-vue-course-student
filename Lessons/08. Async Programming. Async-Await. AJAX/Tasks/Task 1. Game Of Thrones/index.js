c07c9502e853b0a23f612489928e0a821bee5372
alinakamalova204 <alinakamalova919@gmail.com>
2023-10-31 13:41:51 +0300

    Home Work8

--- Lessons/08. Async Programming. Async-Await. AJAX/Tasks/Task 1. Game Of Thrones/index.js
+++ Lessons/08. Async Programming. Async-Await. AJAX/Tasks/Task 1. Game Of Thrones/index.js
@@ -5,7 +5,7 @@ const getCharacterData = (characterId, key) => fetch(BASE_URL + characterId)
   //.then((data) => `${data.name}, ${key}: ${data[key]}`);
     .then((data) => document.body.innerHTML =
        alert(`${data.name} - ${data.gender}`))
-})
+
 getCharacterData(42, 'born').then((info) => {
     console.log(info); // Aegon Targaryen, born: In 281 AC or 282 AC, at Dragonstone
 });
