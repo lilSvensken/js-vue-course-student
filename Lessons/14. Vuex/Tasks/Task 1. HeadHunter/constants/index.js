59192bd0d2fcdd0b3b60131bc482ad6b97b8f04f
elizaveta.shvedchikova <eli3avetta.shvedchikova@gmail.com>
2023-11-06 23:00:11 +0300

    add vue lessons

---
+++ Lessons/14. Vuex/Tasks/Task 1. HeadHunter/constants/index.js
@@ -0,0 +1,76 @@
+export const EDUCATION_LEVEL_OPTIONS = [
+    {
+        "id": "secondary",
+        "name": "Среднее",
+    },
+    {
+        "id": "special_secondary",
+        "name": "Среднее специальное",
+    },
+    {
+        "id": "unfinished_higher",
+        "name": "Неоконченное высшее",
+    },
+    {
+        "id": "higher",
+        "name": "Высшее",
+    },
+    {
+        "id": "bachelor",
+        "name": "Бакалавр",
+    },
+    {
+        "id": "master",
+        "name": "Магистр",
+    },
+    {
+        "id": "candidate",
+        "name": "Кандидат наук",
+    },
+    {
+        "id": "doctor",
+        "name": "Доктор наук",
+    },
+];
+
+export const EXPERIENCE_OPTIONS = [
+    {
+        "id": "noExperience",
+        "name": "Нет опыта",
+    },
+    {
+        "id": "between1And3",
+        "name": "От 1 года до 3 лет",
+    },
+    {
+        "id": "between3And6",
+        "name": "От 3 до 6 лет",
+    },
+    {
+        "id": "moreThan6",
+        "name": "Более 6 лет",
+    },
+];
+
+export const SCHEDULE_OPTIONS = [
+    {
+        "id": "fullDay",
+        "name": "Полный день",
+    },
+    {
+        "id": "shift",
+        "name": "Сменный график",
+    },
+    {
+        "id": "flexible",
+        "name": "Гибкий график",
+    },
+    {
+        "id": "remote",
+        "name": "Удаленная работа",
+    },
+    {
+        "id": "flyInFlyOut",
+        "name": "Вахтовый метод",
+    },
+];
