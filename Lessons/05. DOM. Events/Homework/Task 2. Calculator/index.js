320b9b85d1618b7bb9141b6bcabba2a94683f66f
Cordi216 <nikolaeva2101023@list.ru>
2023-12-11 02:48:55 +0300

    .

--- Lessons/05. DOM. Events/Homework/Task 2. Calculator/index.js
+++ Lessons/05. DOM. Events/Homework/Task 2. Calculator/index.js
@@ -26,7 +26,6 @@ const getSumOfText = (text) => text
   .reduce((acc, num) => acc + Number(num), 0);
 
 const setCalculator = () => {
-<<<<<<< HEAD
     
     const calcMain = document.querySelector('.calc__main');
     const calcButtons = document.querySelector('.calc__buttons');
@@ -82,68 +81,7 @@ const setCalculator = () => {
     };
 
     calcButtons.addEventListener('click', handleButtonClick);
-=======
-    // Начало
-    const textBlock = document.querySelector('.calc__main');
-    const buttonsBlock = document.querySelector('.calc__buttons');
-    
-    let buttonElements = [];
-    for (let i = 0; i <= 9; i += 1) {
-        buttonElements.push({ text: i, type: TYPES.DIGIT });
-    }
-    buttonElements = [ ...buttonElements, ...ADDITIONAL_BUTTONS ];
-    
-    buttonElements.forEach(({ type, text }) => {
-        const button = document.createElement('button');
-        button.classList.add('btn', 'btn-dark');
-        button.dataset.type = type;
-        button.textContent = text;
-        
-        buttonsBlock.append(button);
-    });
-    
-    const resultBlock = document.querySelector('.calc__result');
-    const resultTextElement = document.createElement('span');
-    resultTextElement.textContent = 'Результат: ';
-    
-    const resultNumberElement = document.createElement('span');
-    resultNumberElement.classList.add('calc__result-number');
-    resultNumberElement.textContent = 0;
-    
-    resultBlock.append(resultTextElement, resultNumberElement);
-    
-    const buttons = buttonsBlock.querySelectorAll('button');
-    [ ...buttons ].forEach((button) => {
-        const buttonType = button.dataset.type;
-        
-        if (buttonType === TYPES.DIGIT) {
-            button.addEventListener('click', (event) => {
-                const currentDigit = button.textContent;
-                textBlock.textContent = `${textBlock.textContent}${currentDigit}`;
-            });
-        } else if (buttonType === TYPES.PLUS) {
-            button.addEventListener('click', (event) => {
-                textBlock.textContent = textBlock.textContent + '+';
-            });
-        } else if (buttonType === TYPES.RESULT) {
-            button.addEventListener('click', (event) => {
-                const resultText = textBlock.textContent;
-                const sum = getSumOfText(resultText);
-                
-                const resultBlock = document.querySelector('.calc__result-number');
-                resultBlock.textContent = sum;
-            });
-            
-        } else if (buttonType === TYPES.RESET) {
-            button.addEventListener('click', (event) => {
-                const resultBlock = document.querySelector('.calc__result-number');
-                resultBlock.textContent = 0;
-                textBlock.textContent = '';
-            });
-        }
-    });
-    // Конец
->>>>>>> 9b2875430729b2ac3f51bf430b9da4f65f5dff46
+
 };
 
 export default setCalculator;
