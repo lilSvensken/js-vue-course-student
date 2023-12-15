6b2dfaa5bbcdbc126eec1d5179aa734ce76c3b93
Lazarev <egor0505-lazarev@mail.ru>
2023-12-15 20:01:05 +0300

    Homework 10 first task added

--- Lessons/10. Frameworks 101. Vue.js/Homework/Task 1. Cards/Cards.vue
+++ Lessons/10. Frameworks 101. Vue.js/Homework/Task 1. Cards/Cards.vue
@@ -1,58 +1,93 @@
 <template>
-    <!--Начало-->
-
-    <!--Конец-->
+  <div class="card-container">
+    <div class="card" v-for="card in cards" :key="card.id">
+      <img :src="card.src" class="card-img-top">
+      <div class="card-body">
+        <h5 class="card-title">
+          {{ card.head }}
+        </h5>
+        <h4 class="card-title">
+          {{ card.price }}
+        </h4>
+        <p class="card-text">
+          {{ card.description }}
+        </p>
+        <div class="card__buttons">
+          <button class="btn btn-primary button-plus" @click="plus(card)">
+            +
+          </button>
+          <button class="btn btn-primary button-minus" @click="minus(card)">
+            -
+          </button>
+          <div>{{ card.number }} шт.</div>
+        </div>
+        <div class="card__alert alert alert-danger" v-if="card.number > card.max">
+          Данного товара доступно только {{ card.max }} шт.
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-    name: 'Cards',
-    data() {
-        // Массив данных для карточек товаров уже получен
-        const cards = this.$root.data;
-        return {
-            cards,
-        };
-    },
-    // Начало
+import {c} from "sinon/lib/sinon/spy-formatters";
 
-    // Конец
+export default {
+  name: 'Cards',
+  data() {
+    // Массив данных для карточек товаров уже получен
+    const cards = this.$root.data;
+    return {
+      cards,
+    };
+  },
+  // Начало
+  methods: {
+    plus(card) {
+      card.number++;
+    },
+    minus(card) {
+      if (card.number > 0)
+        card.number--;
+    }
+  }
+  // Конец
 };
 </script>
 
 <style>
 .card-container {
-    display: flex;
-    margin: 40px;
-    padding: 20px;
-    background: aliceblue;
-    width: 1010px;
+  display: flex;
+  margin: 40px;
+  padding: 20px;
+  background: aliceblue;
+  width: 1010px;
 }
 
 .card {
-    width: 300px;
-    margin-right: 15px;
+  width: 300px;
+  margin-right: 15px;
 }
 
 .card img {
-    margin-top: 15px;
+  margin-top: 15px;
 }
 
 .card__buttons {
-    display: flex;
-    align-items: center;
+  display: flex;
+  align-items: center;
 }
 
 .card__buttons button {
-    width: 50px;
-    margin-right: 20px;
+  width: 50px;
+  margin-right: 20px;
 }
 
 .card__buttons div {
-    font-size: 20px;
+  font-size: 20px;
 }
 
 .card__alert {
-    margin-top: 10px;
+  margin-top: 10px;
 }
 </style>
