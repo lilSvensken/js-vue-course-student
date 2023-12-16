5c3a533ba2755df7420191f4ad0d8c123e25ad38
MelnikovaSofya <melsofa@mail.ru>
2023-12-16 22:06:37 +0300

    made homework at 10 lesson

--- Lessons/10. Frameworks 101. Vue.js/Homework/Task 1. Cards/Cards.vue
+++ Lessons/10. Frameworks 101. Vue.js/Homework/Task 1. Cards/Cards.vue
@@ -1,6 +1,35 @@
 <template>
     <!--Начало-->
 
+  <div class="card-container" >
+      <div class="card" v-for="(card, index) in cards" :key="card.id" >
+          <img :src="card.src" class="card-img-top"/>
+          <div class="card-body">
+              <h5 class="card-title">
+                  {{ card.head }}
+              </h5>
+              <h4 class="card-title">
+                  {{ card.price }}
+              </h4>
+              <p class="card-text">
+                  {{ card.description }}
+              </p>
+              <div class="card__buttons" >
+                  <button class="btn btn-primary button-plus" @click="addCount(card.id)">
+                      +
+                  </button>
+                  <button class="btn btn-primary button-minus" @click="removeCount(card.id)">
+                      -
+                  </button>
+                  <div>{{ card.number }} шт.</div>
+              </div>
+              <div v-if="card.number > card.max" class="card__alert alert alert-danger">
+                  Данного товара доступно только {{ card.max }} шт.
+              </div>
+          </div>
+      </div>
+
+  </div>
     <!--Конец-->
 </template>
 
@@ -15,7 +44,19 @@ export default {
         };
     },
     // Начало
+    methods: {
+        addCount(id) {
+            // if (this.cards[id].number < this.cards[id].max) {
+                this.cards[id].number ++;
+            // }
 
+        },
+        removeCount(id) {
+            if (this.cards[id].number > 0) {
+                this.cards[id].number--;
+            }
+        },
+    },
     // Конец
 };
 </script>
--- Lessons/10. Frameworks 101. Vue.js/Homework/Task 2. Carousel Vue/Carousel.vue
+++ Lessons/10. Frameworks 101. Vue.js/Homework/Task 2. Carousel Vue/Carousel.vue
@@ -1,6 +1,23 @@
 <template>
     <!--Начало-->
+    <div class="carousel">
+        <div v-for="(image, index) in images"
+             :key="index"
+            class="carousel-inner">
 
+            <div class="carousel-item " :class="{active: num == index}">
+                <img :src="image"
+                     class="d-block w-100">
+            </div>
+
+        </div>
+        <button class="carousel-control-prev" @click="showPrev">
+            <span class="carousel-control-prev-icon"></span>
+        </button>
+        <button class="carousel-control-next" @click="showNext">
+            <span class="carousel-control-next-icon"></span>
+        </button>
+    </div>
     <!--Конец-->
 </template>
 
@@ -12,10 +29,26 @@ export default {
         const images = this.$root.data;
         return {
             images,
+            num : 0
         };
     },
     // Начало
-
+    methods: {
+        showPrev() {
+            if (this.num > 0) {
+                this.num--;
+            } else {
+                this.num = this.images.length - 1;
+            }
+        },
+        showNext() {
+            if (this.num < this.images.length - 1) {
+                this.num++;
+            } else {
+                this.num = 0;
+            }
+        }
+    }
     // Конец
 };
 </script>
