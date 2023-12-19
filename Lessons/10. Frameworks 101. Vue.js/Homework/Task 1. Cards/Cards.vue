bbf29bd6e953c59ca5bd2b8e8cd6bf355e9264c2
Cordi216 <nikolaeva2101023@list.ru>
2023-12-19 23:06:36 +0300

    Hw 10

--- Lessons/10. Frameworks 101. Vue.js/Homework/Task 1. Cards/Cards.vue
+++ Lessons/10. Frameworks 101. Vue.js/Homework/Task 1. Cards/Cards.vue
@@ -1,6 +1,23 @@
 <template>
     <!--Начало-->
-
+    <div class="card-container">
+    <div v-for="card in cards" :key="card.id" class="card">
+      <img :src="card.src" class="card-img-top" />
+      <div class="card-body">
+        <h5 class="card-title">{{ card.head }}</h5>
+        <h4 class="card-title">{{ card.price }}</h4>
+        <p class="card-text">{{ card.description }}</p>
+        <div class="card__buttons">
+          <button @click="decrement(card)" class="btn btn-primary button-minus">-</button>
+          <button @click="increment(card)" class="btn btn-primary button-plus">+</button>
+          <div>{{ card.number }} шт.</div>
+        </div>
+        <div v-if="card.number > card.max" class="card__alert alert alert-danger">
+          Данного товара доступно только {{ card.max }} шт.
+        </div>
+      </div>
+    </div>
+  </div>
     <!--Конец-->
 </template>
 
@@ -15,7 +32,20 @@ export default {
         };
     },
     // Начало
+    methods: {
+        increment(card) {
+            if (card.number <= card.max){
+                card.number++;
+            }
+        },
+        decrement(card){
+            if (card.number > 0) {
+                card.number--;
+            }
+        }
 
+
+    }
     // Конец
 };
 </script>
