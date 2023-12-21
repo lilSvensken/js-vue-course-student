fae9ce377fa5c88494a5d911da5ca3aaf64cebfe
KasimovDanil637 <kasimovdanil637@gmail.com>
2023-12-21 18:18:14 +0300

    add homework_11

--- Lessons/11. Vue. Reactivity. VDOM. Emits. Props/Homework/Task 1. Recipes/Recipes.vue
+++ Lessons/11. Vue. Reactivity. VDOM. Emits. Props/Homework/Task 1. Recipes/Recipes.vue
@@ -1,32 +1,47 @@
 <template>
-    <div class="recipes">
-        <!--Начало-->
-
-        <!--Конец-->
-    </div>
+  <!--Начало-->
+  <div class="recipes">
+    <NewRecipeForm :recipe="recipes"/>
+    <RecipesContainer :recipes="recipes" @deleteRecipe="deleteRecipe"/>
+  </div>
+  <!--Конец-->
 </template>
 
 <script>
+import NewRecipeForm from  './components/NewRecipeForm.vue';
+import RecipesContainer from  './components/RecipesContainer.vue';
 export default {
-    name: 'Recipes',
-    // Начало
-
-    // Конец
+  name: 'Recipes',
+  data() {
+    return {
+      recipes: [],
+    }
+  },
+  components: { NewRecipeForm, RecipesContainer,},
+  methods: {
+    deleteRecipe(recipe) {
+      const index = this.recipes.indexOf(recipe);
+      if (index !== -1) {
+        this.recipes.splice(index, 1);
+      }
+    }
+  },
+  // Конец
 };
 </script>
 
 <style>
 .recipes {
-    width: 1000px;
-    margin: 40px;
-    display: flex;
-    justify-content: center;
-    padding: 20px 0;
-    background: whitesmoke;
-    border-radius: 15px;
+  width: 1000px;
+  margin: 40px;
+  display: flex;
+  justify-content: center;
+  padding: 20px 0;
+  background: whitesmoke;
+  border-radius: 15px;
 }
 
 .recipes > div {
-    width: 50%;
+  width: 50%;
 }
 </style>
