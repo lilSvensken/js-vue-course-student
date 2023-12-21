e34e5ff6763967bf8085fef4353a1042233cb241
Lazarev <egor0505-lazarev@mail.ru>
2023-12-22 00:21:01 +0300

    Homework 11 added

--- Lessons/11. Vue. Reactivity. VDOM. Emits. Props/Homework/Task 1. Recipes/Recipes.vue
+++ Lessons/11. Vue. Reactivity. VDOM. Emits. Props/Homework/Task 1. Recipes/Recipes.vue
@@ -1,16 +1,50 @@
 <template>
     <div class="recipes">
         <!--Начало-->
-
-        <!--Конец-->
+      <NewRecipeForm @add="addNewRecipe"></NewRecipeForm>
+      <div class="recipe-container">
+        <h3>
+          Мои рецепты
+        </h3>
+        <RecipesContainer v-for="(recipe,index) in recipes" :recipe-name="recipe.name" :recipe-ingredients="recipe.ingredients" :recipe-text="recipe.text" :recipe-cook-time="recipe.cookTime" :recipe-vegan="recipe.vegan" @delete="deleteRecipe(index)"></RecipesContainer>
+        <div class="recipe-container__no-recipes" v-if="recipes.length === 0">
+            Рецептов нет
+        </div>
+    </div>
     </div>
 </template>
 
 <script>
+import NewRecipeForm from "./components/NewRecipeForm.vue";
+import RecipesContainer from "./components/RecipesContainer.vue";
+
 export default {
     name: 'Recipes',
+  components: {RecipesContainer, NewRecipeForm},
     // Начало
+  data(){
 
+      return {
+        recipes: [
+        ],
+      };
+  },
+  methods: {
+      addNewRecipe(recipeName, recipeIngredients, recipeText, recipeCookTime, recipeVegan){
+        let vegan = "нет";
+        if (recipeVegan){
+          vegan = "да"
+        }
+        this.recipes.push({name: recipeName, ingredients: recipeIngredients, text: recipeText, cookTime: recipeCookTime, vegan: vegan});
+        console.log(this.recipes);
+      },
+    deleteRecipe(index) {
+        console.log("del")
+      console.log(index)
+        this.recipes.splice(index, 1);
+        console.log(this.recipes)
+    }
+  }
     // Конец
 };
 </script>
@@ -28,5 +62,9 @@ export default {
 
 .recipes > div {
     width: 50%;
+}
+.recipe-container__no-recipes {
+  font-size: 20px;
+  margin-top: 15px;
 }
 </style>
--- Lessons/11. Vue. Reactivity. VDOM. Emits. Props/Homework/Task 1. Recipes/components/NewRecipeForm.vue
+++ Lessons/11. Vue. Reactivity. VDOM. Emits. Props/Homework/Task 1. Recipes/components/NewRecipeForm.vue
@@ -1,8 +1,46 @@
 <template>
     <div class="recipe-form">
-        <!--Начало-->
-
-        <!--Конец-->
+      <h3>
+        Создать новый рецепт
+      </h3>
+      <div class="recipe-form__block">
+        <label class="form-label">
+          Название нового рецепта
+        </label>
+        <input type="text" class="form-control recipe-form__name" ref="recipeName">
+      </div>
+      <div class="recipe-form__block">
+        <label class="form-label">
+          Ингредиенты
+        </label>
+        <textarea class="form-control recipe-form__ingredients" ref="recipeIngredients"></textarea>
+      </div>
+      <div class="recipe-form__block">
+        <label class="form-label">
+          Последовательность действий
+        </label>
+        <textarea class="form-control recipe-form__recipe-text" ref="recipeText"></textarea>
+      </div>
+      <div class="recipe-form__block"><label class="form-label">
+        Время приготовления
+      </label>
+        <select class="form-select recipe-form__cook-time" ref="recipeCookTime">
+          <option :value="timeOption.value" v-for="timeOption in timeOptions">
+            {{timeOption.text}}
+          </option>
+        </select>
+      </div>
+      <div class="recipe-form__block">
+        <input type="checkbox" class="form-check-input" ref="recipeVegan">
+        <label class="form-check-label">
+          Вегетарианское блюдо
+        </label>
+      </div>
+      <div class="recipe-form__block">
+        <button class="btn btn-dark recipe-form__add-button" @click="sendForm()">
+          Добавить рецепт
+        </button>
+      </div>
     </div>
 </template>
 
@@ -10,9 +48,18 @@
 export default {
     name: 'NewRecipeForm',
     data() {
-        return {
+      let recipeName;
+      let recipeIngredients;
+      let recipeText;
+      let recipeCookTime;
+      let recipeVegan;
+      return {
             // Начало
-
+        recipeName,
+        recipeIngredients,
+        recipeText,
+        recipeCookTime,
+        recipeVegan,
             // Конец
             // Опции для селекта "Время приготовления"
             timeOptions: [
@@ -33,7 +80,18 @@ export default {
         };
     },
     // Начало
-
+  emits: ["add"],
+  methods: {
+      sendForm() {
+        this.recipeName = this.$refs.recipeName.value;
+        console.log(this.recipeName)
+        this.recipeIngredients = this.$refs.recipeIngredients.value;
+        this.recipeText = this.$refs.recipeText.value;
+        this.recipeCookTime = this.$refs.recipeCookTime.options[this.$refs.recipeCookTime.selectedIndex].text;
+        this.recipeVegan = this.$refs.recipeVegan.checked;
+        this.$emit("add", this.recipeName, this.recipeIngredients, this.recipeText, this.recipeCookTime, this.recipeVegan);
+      },
+  }
     // Конец
 };
 </script>
