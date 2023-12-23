593c075a246cbb3e69b85761fe0719cef1860c52
Lazarev <egor0505-lazarev@mail.ru>
2023-12-23 10:07:53 +0300

    Homework 10 second task added

--- Lessons/10. Frameworks 101. Vue.js/Homework/Task 2. Carousel Vue/Carousel.vue
+++ Lessons/10. Frameworks 101. Vue.js/Homework/Task 2. Carousel Vue/Carousel.vue
@@ -1,7 +1,17 @@
 <template>
-    <!--Начало-->
-
-    <!--Конец-->
+  <div class="carousel">
+    <div class="carousel-inner" v-for="(image, index) in images">
+      <div class="carousel-item" :class="{active : isActive(index)}" >
+        <img :src="image" class="d-block w-100">
+      </div>
+    </div>
+    <button class="carousel-control-prev" @click="previous()">
+      <span class="carousel-control-prev-icon"></span>
+    </button>
+    <button class="carousel-control-next" @click="next()">
+      <span class="carousel-control-next-icon"></span>
+    </button>
+  </div>
 </template>
 
 <script>
@@ -10,12 +20,32 @@ export default {
     data() {
         // Массив картинок уже получен
         const images = this.$root.data;
+        let activeImageIndex = 0;
         return {
             images,
+          activeImageIndex,
         };
     },
     // Начало
-
+  methods: {
+      next() {
+        if (this.activeImageIndex === this.images.length-1){
+          this.activeImageIndex = 0;
+        } else {
+          this.activeImageIndex = this.activeImageIndex+1;
+        }
+      },
+    previous(){
+      if (this.activeImageIndex === 0){
+        this.activeImageIndex = this.images.length-1;
+      } else {
+        this.activeImageIndex = this.activeImageIndex-1;
+      }
+    },
+    isActive(index) {
+        return (index === this.activeImageIndex)
+    }
+  }
     // Конец
 };
 </script>
