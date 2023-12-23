99ef881cfd017aacc90109c1819e0f61de293b33
Impax666 <immortalyfleks@gmail.com>
2023-12-23 13:14:36 +0300

    HW 10 + HW 12

--- Lessons/10. Frameworks 101. Vue.js/Homework/Task 1. Cards/Cards.vue
+++ Lessons/10. Frameworks 101. Vue.js/Homework/Task 1. Cards/Cards.vue
@@ -1,58 +1,84 @@
 <template>
-    <!--Начало-->
-
-    <!--Конец-->
+  <div class="card-container">
+    <div class="card" v-for="card in cards" :key="card.id">
+      <img :src="card.src" class="card-img-top"/>
+      <div class="card-body">
+        <h5 class="card-title">{{ card.head }}</h5>
+        <h4 class="card-title">{{ card.price }}</h4>
+        <p class="card-text">{{ card.description }}</p>
+        <div class="card\_\_buttons">
+          <button class="btn btn-primary button-plus" @click="addCounter(card.id)">+</button>
+          <button class="btn btn-primary button-minus" @click="decreaseCounter(card.id)">-</button>
+          <div>{{ card.number }}</div>
+        </div>
+        <div class="card\_\_alert alert alert-danger" v-if="!decreaseIsAvailable(card.id)">
+          Данного товара доступно только {{ card.max }} шт.
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Cards',
-    data() {
-        // Массив данных для карточек товаров уже получен
-        const cards = this.$root.data;
-        return {
-            cards,
-        };
+  name: 'Cards',
+  data() {
+    const cards = this.$root.data;
+    return {
+      cards,
+    };
+  },
+  methods: {
+    addCounter(id) {
+      if (this.decreaseIsAvailable(id) === true) {
+        this.cards.find(card => card.id === id).number++;
+      }
     },
-    // Начало
-
-    // Конец
+    decreaseCounter(id) {
+      if (this.cards.find(card => card.id === id).number === 0) {
+        return;
+      } this.cards.find(card => card.id === id).number--;
+    },
+    decreaseIsAvailable(id) {
+      return this.cards.find(card => card.id === id).number < this.cards.find(card => card.id === id).max;
+    },
+  }
 };
 </script>
 
 <style>
 .card-container {
-    display: flex;
-    margin: 40px;
-    padding: 20px;
-    background: aliceblue;
-    width: 1010px;
+  display: flex;
+  margin: 40px;
+  padding: 20px;
+  background: aliceblue;
+  width: 1010px;
 }
 
 .card {
-    width: 300px;
-    margin-right: 15px;
+  width: 300px;
+  margin-right: 15px;
 }
 
 .card img {
-    margin-top: 15px;
+  margin-top: 15px;
 }
 
 .card__buttons {
-    display: flex;
-    align-items: center;
+  display: flex;
+  align-items: center;
 }
 
 .card__buttons button {
-    width: 50px;
-    margin-right: 20px;
+  width: 50px;
+  margin-right: 20px;
 }
 
 .card__buttons div {
-    font-size: 20px;
+  font-size: 20px;
 }
 
 .card__alert {
-    margin-top: 10px;
+  margin-top: 10px;
 }
 </style>
--- Lessons/10. Frameworks 101. Vue.js/Homework/Task 2. Carousel Vue/Carousel.vue
+++ Lessons/10. Frameworks 101. Vue.js/Homework/Task 2. Carousel Vue/Carousel.vue
@@ -1,42 +1,57 @@
 <template>
-    <!--Начало-->
-
-    <!--Конец-->
+  <div class="carousel">
+    <div class="carousel-inner">
+      <div v-for="(image, index) in images" :key="index" :class="{ 'carousel-item' : true, 'active' : index === currentSlide}">
+        <img :src="image" class="d-block w-100" />
+      </div>
+    </div>
+    <button class="carousel-control-prev" @click="prevSlide">
+      <span class="carousel-control-prev-icon"></span>
+    </button>
+    <button class="carousel-control-next" @click="nextSlide">
+      <span class="carousel-control-next-icon"></span>
+    </button>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Carousel',
-    data() {
-        // Массив картинок уже получен
-        const images = this.$root.data;
-        return {
-            images,
-        };
+  name: 'Carousel',
+  data() {
+    const images = this.$root.data;
+    return {
+      images,
+      currentSlide: 0
+    };
+  },
+  methods: {
+    nextSlide() {
+      this.currentSlide = (this.currentSlide + 1) % this.images.length;
     },
-    // Начало
-
-    // Конец
+    prevSlide() {
+      this.currentSlide = this.currentSlide === 0 ? this.images.length - 1 : this.currentSlide - 1;
+    }
+  }
 };
 </script>
 
 <style>
 .carousel {
-    background: #f6f6f6;
-    display: block;
-    padding: 30px;
-    margin: 40px;
-    font-family: Verdana, Tahoma, sans-serif;
-    border-radius: 10px;
-    align-items: center;
-    width: 1000px;
+  background: #f6f6f6;
+  display: block;
+  padding: 30px;
+  margin: 40px;
+  font-family: Verdana, Tahoma, sans-serif;
+  border-radius: 10px;
+  align-items: center;
+  width: 1000px;
 }
 
 .carousel-item {
-    min-height: 325px;
+  min-height: 325px;
 }
 
 .carousel-item.active {
-    display: flex;
+  display: flex;
 }
 </style>
